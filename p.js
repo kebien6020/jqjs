125c1574af20e60c9eddc2306c662f0a7e8e5649
Kevin Peña <kevin.pena.prog@gmail.com>
2023-10-16 23:15:50 -0500

    Update p.js

--- p.js
+++ p.js
@@ -1,13 +1,15 @@
+#!/usr/bin/env node
 // Like jq, but worse
 // curl -L https://raw.githubusercontent.com/kebien6020/jqjs/main/p.js -o p.js
 // find . -maxdepth 1 -type f | node -e 'with(require("./p.js")){r()}'
 
 const fs = require('fs')
+const util = require('util')
 
 // in
 const i = () => fs.readFileSync(0, 'utf8')
 // out
-const o = d => (console.log(d), d)
+const o = d => (console.log(util.inspect(d, false, null, true)), d)
 // out as json
 const oj = d => console.log(JSON.stringify(d, null, 2))
 // pipe
@@ -36,9 +38,14 @@ const window = l => d => d.flatMap((_, i) => i <= d.length - l ? [d.slice(i, i +
 const sort = comp => d => d.sort(comp)
 const compare = (a, b) => a < b ? -1 : a > b ? 1 : 0
 const by = proj => (a, b) => compare(proj(a), proj(b))
+const plus = (a, b) => a + b
+const sum = reduce (plus) (0)
+const min = (a, b) => a < b ? a : b
+const minimum = reduce (min) (Infinity)
 
 module.exports = {
 	i, o, oj, c, p, r, rj,
 	map, reduce, filter, group_by, reverse, split, join, lines, unlines,
-	first, last, enumerate, flat, window, sort, compare, by
+	first, last, enumerate, flat, window, sort, compare, by,
+	plus, sum, min, minimum,
 }
